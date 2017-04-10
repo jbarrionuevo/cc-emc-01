204b9f6da4556ca55469b1d0d5f56e427a8d9b46
jbarrionuevo <jbarrionuevo@gmail.com>
2017-04-09 23:23:32 -0300

    Adds a call center test to verify no employee available scenario

--- src/test/java/AllTests.java
+++ src/test/java/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DispatcherTest.class, EmployeeTest.class, CallCenterTest.class })
+@SuiteClasses({ DispatcherTest.class, EmployeeTest.class, CallCenterTest.class, CallCenterTwoEmployeesTest.class })
 public class AllTests {
 
 }
--- src/test/java/CallCenterTwoEmployeesTest.java
+++ src/test/java/CallCenterTwoEmployeesTest.java
@@ -9,44 +9,25 @@ import main.java.model.Customer;
 import main.java.model.Employee;
 
 /**
- * Main test case that runs 10 calls with a MainController.
+ * Main test case that runs 10 calls with a MainController and only two
+ * available employees to take calls.
  * 
  * @author Jorge
  *
  * @param <E>
  */
-public class CallCenterTest<E> {
+public class CallCenterTwoEmployeesTest<E> {
 	private MainController<Employee> mainController;
 
 	@Before
 	public void setUp() throws Exception {
 		// create employees
 		Employee s1 = new Employee("Laurent", "supervisor");
-		Employee s2 = new Employee("Laurent", "supervisor");
-		Employee d1 = new Employee("Rolph", "director");
 		Employee o1 = new Employee("Celeste", "operator");
-		Employee o2 = new Employee("Rolph", "operator");
-		Employee o3 = new Employee("Ramon", "operator");
-		Employee o4 = new Employee("Cecil", "operator");
-		Employee o5 = new Employee("Keith", "operator");
-		Employee o6 = new Employee("Ramiro", "operator");
-		Employee o7 = new Employee("Charles", "operator");
-		Employee o8 = new Employee("Robert", "operator");
-		Employee o9 = new Employee("Zoe", "operator");
 		// create a callcenter and add employees
 		CallCenter<Employee> callCenter = new CallCenter<Employee>();
-		callCenter.addEmployeeToShift(s1);
-		callCenter.addEmployeeToShift(s2);
-		callCenter.addEmployeeToShift(d1);
 		callCenter.addEmployeeToShift(o1);
-		callCenter.addEmployeeToShift(o2);
-		callCenter.addEmployeeToShift(o3);
-		callCenter.addEmployeeToShift(o4);
-		callCenter.addEmployeeToShift(o5);
-		callCenter.addEmployeeToShift(o6);
-		callCenter.addEmployeeToShift(o7);
-		callCenter.addEmployeeToShift(o8);
-		callCenter.addEmployeeToShift(o9);
+		callCenter.addEmployeeToShift(s1);
 		// create customers
 		Customer c1 = new Customer("Claude");
 		Customer c2 = new Customer("John");
